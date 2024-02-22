b63306e146b9f4d0fb691f03ece523d271ef0f40
Yogita Shete <yogitashete24@gmail.com>
2024-02-22 11:19:49 +0530

    ref : server and client seperated

--- client/src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import About from "../views/About/About";
-import Developement from "../views/Developement/Developement";
-import FAQ from "../views/FAQ/FAQ";
-import Home from "../views/Home/Home";
-import Login from "../views/Login/Login";
-import Signup from "../views/Signup/Signup";
-
-export { About, Developement, FAQ, Home, Login, Signup };
