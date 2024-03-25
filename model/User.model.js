bf1439de7cdd90789a4f90d75ccde45ca473aff2
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-03-25 16:10:21 +0530

    ref: Add user model with bcrypt password hashing

--- model/User.model.js
+++ model/User.model.js
@@ -1,20 +1,28 @@
 import { Schema, model } from "mongoose";
-
+import bcrypt from "bcrypt"
 const UserSchema = new Schema(
   {
     name: {
       type: String,
       required: [true, "Please enter your name."],
+      trim: true,
+      index: true,
     },
 
     username: {
       type: String,
       unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
 
     email: {
       type: String,
       required: [true, "Please enter your email."],
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
 
     mobile: {
@@ -27,12 +35,15 @@ const UserSchema = new Schema(
     },
 
     avatar: {
-      type: String,
+      type: String, // cloudinary url
     },
 
     password: {
       type: String,
       required: [true, "Please enter the strong password."],
+    },
+    refreshToken: {
+      type: String,
     },
 
     socialMedia: {
@@ -60,9 +71,25 @@ const UserSchema = new Schema(
         type: String,
       },
     },
+    MyVideo: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
   },
 
   { timestamps: true }
 );
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+UserSchema.methods.isPasswordCorrect = async function (password) {
+ return await bcrypt.compare(password, this.password)
+};
 const User = model("User", UserSchema);
 export default User;
