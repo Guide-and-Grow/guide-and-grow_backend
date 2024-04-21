e84685d588490d69c519b00dfdbed385533f7693
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-04-21 13:51:40 +0530

    fixed : "Change from res.user to req.user in getCurrentUser controller"

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -230,7 +230,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 });
 
 const getCurrentUser = asyncHandler(async (req, res) => {
-  return res.status(200).json(new ApiResponse(200, res.user, "User fetched"));
+  return res.status(200).json(new ApiResponse(200, req.user, "User fetched"));
 });
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
